ac561ecd4f111aeec0c5e9c2199bb706ca1e12d9
Salvatore Munaò <salvatoremunao05@gmail.com>
2023-06-16 21:59:07 +0200

    Aggiunta definizione per gestire le pagine non trovate

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
  res.send('Hello from Node.js server');
 });
 
+// Definizione di una route di fallback per gestire le pagine non trovate
+app.use((req, res) => {
+ res.status(404).send("Spiacenti, la pagina richiesta non è stata trovata.");
+});
+
+
 
         //comando che ci permette di far partire il nostro server e quindi l'applicazione
 const server = app.listen(process.env.PORT || 8000, () => {
